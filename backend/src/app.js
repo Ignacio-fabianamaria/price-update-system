3320bcaf3ae30531d571a4735aba159a70280b5f
Siigmaf <faby.tbe@gmail.com>
2023-05-13 20:13:58 -0300

    feat(database): Adiciona docker-compose e implementa conexão com MySQL

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (rep, res) => {
+    res.status(200).json({message: 'olááá!!!!'})
+});
+
 module.exports = {
     app,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,14 @@
 const { app } = require('./app');
+const connection = require('../src/db/dumps/connection.js');
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+  
+    // O código abaixo é para testarmos a comunicação com o MySQL
+    const [result] = await connection.execute('SELECT 1');
+    if (result) {
+      console.log('MySQL connection OK');
+    }
+  });
